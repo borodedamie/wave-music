572967f3cba0e677957b87839bc7dd1cda2f2f8f
Onaopemipo Oluborode <opeoluborode@yahoo.com>
2024-02-02 18:51:58 +0100

    started desktop design - completed navbar, hero section, and about page

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+function Hero() {
+    return (
+        <div className="hero">
+            <h1>Begin Today On The Path To Success As A Music Producer.</h1>
+            <p>Lorem ipsum dolor sit amet consectetur. Non molestie nisl sapien donec congue
+                aliquet lectus pellentesque. Id sed posuere diam arcu amet aliquam nec libero.
+                Eget platea.</p>
+            <button>Enroll Now</button>
+        </div>
+    )
+};
+
+export default Hero;
